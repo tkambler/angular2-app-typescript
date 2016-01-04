3ae78a8b1964b5823e7abf0dbdb0936e7da2c6dd
Tim Ambler <tkambler@gmail.com>
2016-01-03 18:42:12 -0600

    Automatically open browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,33 @@ module.exports = (grunt) => {
                 'script': 'bin/server.js',
                 'options': {
                     'watch': ['server/**/*', 'server_config/**/*'],
-                    'delay': 1000
+                    'delay': 1000,
+                    'callback': (nodemon) => {
+
+                        nodemon.on('config:update', () => {
+                            var config = require('app/config');
+                            setTimeout(() => {
+                                require('open')('http://localhost:' + config.get('port'));
+                            }, 1000);
+                        });
+
+                    }
                 }
             }
         }
     });
 
+    grunt.registerTask('load-config', function() {
+        var done = this.async();
+        var path = require('path');
+        require('app/config')(path.join(__dirname, 'server_config'))
+            .then(() => {
+                return done();
+            });
+    });
+
     grunt.registerTask('default', ['nodemon']);
+
+    grunt.task.run(['load-config']);
 
 };
